9e3a07b5897a70cc2c177c2144cbf68ba4675702
Sasha Turchanskiy <turchanskiy777@gmail.com>
2025-08-26 11:53:36 +0300

    Added token configuration and security

--- src/main/java/com/zosh/treading/controller/HomeController.java
+++ src/main/java/com/zosh/treading/controller/HomeController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping
     public String home() {
         return "Welcome to the Home Page!";
     }
