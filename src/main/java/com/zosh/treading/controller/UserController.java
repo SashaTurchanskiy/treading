3d50200dbead49e7b79dc448e03cb37d9d38949f
Sasha Turchanskiy <turchanskiy777@gmail.com>
2025-09-22 13:45:58 +0300

    Finished working with verify otp part

--- src/main/java/com/zosh/treading/controller/UserController.java
+++ src/main/java/com/zosh/treading/controller/UserController.java
@@ -2,8 +2,11 @@ package com.zosh.treading.controller;
 
 import com.zosh.treading.domain.VerificationType;
 import com.zosh.treading.model.User;
+import com.zosh.treading.model.VerificationCode;
 import com.zosh.treading.service.EmailService;
 import com.zosh.treading.service.UserService;
+import com.zosh.treading.service.VerificationCodeService;
+import com.zosh.treading.service.VerificationCodeServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,7 @@ public class UserController {
     private final UserService userService;
     private final EmailService emailService;
     private String jwt;
+    private final  VerificationCodeService verificationCodeService;
 
     @GetMapping("/profile")
     public ResponseEntity<User> getUserProfile(@RequestHeader("Authorization") String jwt) throws Exception {
@@ -25,11 +29,34 @@ public class UserController {
         return new ResponseEntity<User>(user, HttpStatus.OK);
     }
     @PostMapping("/send-otp/{verificationType}")
-    public ResponseEntity<User> sendVerificationOtp(@RequestHeader("Authorization") String jwt,
+    public ResponseEntity<String> sendVerificationOtp(@RequestHeader("Authorization") String jwt,
                                                     @PathVariable VerificationType verificationType) throws Exception {
-
         User user = userService.findUserProfileByJwt(jwt);
-        return new ResponseEntity<User>(user, HttpStatus.OK);
+
+        VerificationCode verificationCode = verificationCodeService.getVerificationCodeByUser(user.getId());
+        if(verificationCode == null){
+            verificationCode = verificationCodeService.sendVerificationCode(user, verificationType);
+        }
+        if (verificationType.equals(VerificationType.EMAIL)){
+            emailService.sendVerificationOtpEmail(user.getEmail(), verificationCode.getOtp());
+        }
+
+        return new ResponseEntity<>("verification otp send successfully", HttpStatus.OK);
+    }
+    @PutMapping("/api/users/enable-2fa/{otp}")
+    public ResponseEntity<User> enableTwoFactorAuthentication(@PathVariable String otp,
+                                                              @RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        VerificationCode verificationCode = verificationCodeService.getVerificationCodeByUser(user.getId());
+        String sendTo = verificationCode.getVerificationType().equals(VerificationType.EMAIL)?
+                verificationCode.getEmail() : verificationCode.getMobile();
+        boolean isVerified = verificationCode.getOtp().equals(otp);
+        if (isVerified){
+            User updatedUser = userService.enableTwoFactorAuthentication(verificationCode.getVerificationType(), sendTo, user);
+            verificationCodeService.deleteVerificationCode(verificationCode.getId()); // видаляємо код після успішної верифікації
+            return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+        }
+        throw new Exception("Invalid OTP");
     }
 
 
