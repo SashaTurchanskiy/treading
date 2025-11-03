bd1d8a4f23de4ddea65e830660585080c340726e
Sasha Turchanskiy <turchanskiy777@gmail.com>
2025-11-03 12:38:00 +0200

    finished working with impl part, started working with controller

--- src/main/java/com/zosh/treading/service/PaymentService.java
+++ src/main/java/com/zosh/treading/service/PaymentService.java
@@ -1,6 +1,7 @@
 package com.zosh.treading.service;
 
 import com.razorpay.RazorpayException;
+import com.stripe.exception.StripeException;
 import com.zosh.treading.domain.PaymentMethod;
 import com.zosh.treading.model.PaymentOrder;
 import com.zosh.treading.model.User;
@@ -10,6 +11,6 @@ public interface PaymentService {
     PaymentOrder createOrder(User user, Long amount, PaymentMethod paymentMethod);
     PaymentOrder getPaymentOrderById(Long id) throws Exception;
     Boolean ProcessPaymentOrder(PaymentOrder paymentOrder, String paymentId) throws RazorpayException;
-    PaymentResponse createRazorpayPaymentLink(User user, Long amount);
-    PaymentResponse createStripePaymentLink(User user, Long amount, Long orderId);
+    PaymentResponse createRazorpayPaymentLink(User user, Long amount) throws RazorpayException;
+    PaymentResponse createStripePaymentLink(User user, Long amount, Long orderId) throws StripeException;
 }
--- src/main/java/com/zosh/treading/service/PaymentServiceImpl.java
+++ src/main/java/com/zosh/treading/service/PaymentServiceImpl.java
@@ -1,8 +1,13 @@
 package com.zosh.treading.service;
 
 import com.razorpay.Payment;
+import com.razorpay.PaymentLink;
 import com.razorpay.RazorpayClient;
 import com.razorpay.RazorpayException;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
 import com.zosh.treading.domain.PaymentMethod;
 import com.zosh.treading.domain.PaymentOrderStatus;
 import com.zosh.treading.model.PaymentOrder;
@@ -10,6 +15,7 @@ import com.zosh.treading.model.User;
 import com.zosh.treading.repository.PaymentOrderRepo;
 import com.zosh.treading.response.PaymentResponse;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -68,12 +74,85 @@ public class PaymentServiceImpl implements PaymentService {
     }
 
     @Override
-    public PaymentResponse createRazorpayPaymentLink(User user, Long amount) {
-        return null;
+    public PaymentResponse createRazorpayPaymentLink(User user, Long amount) throws RazorpayException {
+
+        Long Amount  = amount * 100;
+
+        try {
+            //Instantiate Razorpay Client
+            RazorpayClient razorpayClient = new RazorpayClient(apiKey, apiSecretKey);
+
+            //Create Order JSON Object
+            JSONObject paymentLinkRequest = new JSONObject();
+            paymentLinkRequest.put("amount", Amount);
+            paymentLinkRequest.put("currency", "INR");
+
+            JSONObject customer = new JSONObject();
+            customer.put("name", user.getFullName());
+
+            customer.put("email", user.getEmail());
+            paymentLinkRequest.put("customer", customer);
+
+            //create a json object for notify
+            JSONObject notify = new JSONObject();
+            notify.put("email", true);
+            paymentLinkRequest.put("notify", notify);
+
+            //send the reminder setting
+            paymentLinkRequest.put("reminder_enable", true);
+
+            //set the callback URl and method
+            paymentLinkRequest.put("callback_url", "http://localhost:5173/wallet");
+            paymentLinkRequest.put("callback_method", "get");
+
+            //create the payment link using the paymentLink.create() method
+            PaymentLink payment = razorpayClient.paymentLink.create(paymentLinkRequest);
+
+            String paymentLinkId = payment.get("id");
+            String paymentLinkUrl = payment.get("short_url");
+
+            PaymentResponse paymentResponse = new PaymentResponse();
+            paymentResponse.setPayment_url(paymentLinkUrl);
+            return paymentResponse;
+        } catch (RazorpayException e) {
+            System.out.println("Error while creating Razorpay payment link: " + e.getMessage());
+            throw new RazorpayException(e.getMessage());
+        }
     }
 
     @Override
-    public PaymentResponse createStripePaymentLink(User user, Long amount, Long orderId) {
-        return null;
+    public PaymentResponse createStripePaymentLink(User user, Long amount, Long orderId) throws StripeException {
+
+        Stripe.apiKey = stripeSecretKey;
+
+        SessionCreateParams params = SessionCreateParams.builder()
+                .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .setSuccessUrl("http://localhost:5173/wallet?order_id= + orderId")
+                .setCancelUrl("http://localhost:5173/payment/cancel")
+                .addLineItem(SessionCreateParams.LineItem.builder()
+                        .setQuantity(1L)
+                        .setPriceData(SessionCreateParams.LineItem.PriceData.builder()
+                                .setCurrency("usd")
+                                .setUnitAmount(amount * 100)
+                                .setProductData(SessionCreateParams
+                                        .LineItem
+                                        .PriceData
+                                        .ProductData
+                                        .builder()
+                                        .setName("Top up wallet")
+                                        .build()
+                                ).build()
+                        ).build()
+                ).build();
+
+        Session session = Session.create(params);
+
+        System.out.println("session _____" + session);
+
+        PaymentResponse paymentResponse = new PaymentResponse();
+        paymentResponse.setPayment_url(session.getUrl());
+
+        return paymentResponse;
     }
 }
